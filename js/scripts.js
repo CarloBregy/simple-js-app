f671b20a908f544f5cd1afa6f7facc37e92bbd13
CB <carlo_bregy@outlook.com>
2025-07-01 11:59:46 +0200

    Added Task 1.3
    I added for loop code to the document.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,79 @@
-var pokemonList = [{name: "Bulbasaur", height: "0.7", type: ["grass", "poison"]},
-                   {name: "Ivysaur", height: "1", type: ["grass", "poison"]},
-                   {name: "Venusaur", height: "2", type: ["grass", "poison"]},
-                   {name: "Charmander", height: "0.6", type: ["fire", "dragon"]}]
+var pokemonList = [{ name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+{ name: "Ivysaur", height: 1, type: ["grass", "poison"] },
+{ name: "Venusaur", height: 2.2, type: ["grass", "poison"] },
+{ name: "Charmander", height: 0.6, type: ["fire", "dragon"] }]
+
+
+
+for (let i = 0; i < pokemonList.length; i++){
+    document.write(pokemonList[i].name + " " + (pokemonList[i].height));
+    if (pokemonList[i].height >2 ) {
+        document.write(" - This is big");     // If the height is greater than 2, print a message
+        }
+        document.write("<br>");               // Move to next line after each Pokémon
+    }
+
+
+
+
+
+
+// let day = "Sunday";
+
+// if  (day === "Sunday") {
+//     console.log("It's Sunday!");}
+// else if (day === "Saturday") {
+//     console.log("It's Saturday!");} 
+// else {
+//     console.log("t’s not the weekend! Boo!");}    
+
+
+// let personAge = [
+//     {name: "person1", age: 16}, 
+//     {name: "person2", age: 10}, 
+//     {name: "person3", age: 25},
+//     ];
+
+// for (let i=0; i < personAge.length; i++){
+//     if (personAge[i].age <19 && personAge[i].age >13){
+//         console.log(personAge[i].name + " is a teenager")
+//     }else if (personAge[i].age <13) {
+//         console.log(personAge[i].name + " is a child")
+//     }else {
+//         console.log(personAge[i].name + ' is an adult');
+//     }
+// }
+
+
+
+//  let text = "";
+//  let i = 0;
+
+//  do {
+//     text = text + " " + i;
+//     i++;
+//  }
+//  while (i < 10);
+//  console.log(text);
+
+
+// let fruits = ["apple", "banana", "cherry", "elderberry"]
+// let text = "";
+// let i = 0;
+
+// while (fruits[i]) {
+//     text = text + " " + fruits[i];
+//     i++;
+// }
+
+// console.log(text);
+    
+// let fruits = ["apple", "banana", "cherry", "elderberry"]
+// let text = "";
+// let i = 0;
+// for (;fruits[i];) {
+//     text = text + " " + fruits[i];
+//     i++;
+// }
+
+// console.log(text);
