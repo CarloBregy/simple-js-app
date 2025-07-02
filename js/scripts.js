525e999b83c351e65add07bf92abe1e17e48db0c
CB <carlo_bregy@outlook.com>
2025-07-02 16:52:37 +0200

    Delete unnecessary code

--- js/scripts.js
+++ js/scripts.js
@@ -18,62 +18,8 @@ for (let i = 0; i < pokemonList.length; i++){
 
 
 
-// let day = "Sunday";
-
-// if  (day === "Sunday") {
-//     console.log("It's Sunday!");}
-// else if (day === "Saturday") {
-//     console.log("It's Saturday!");} 
-// else {
-//     console.log("t’s not the weekend! Boo!");}    
-
-
-// let personAge = [
-//     {name: "person1", age: 16}, 
-//     {name: "person2", age: 10}, 
-//     {name: "person3", age: 25},
-//     ];
-
-// for (let i=0; i < personAge.length; i++){
-//     if (personAge[i].age <19 && personAge[i].age >13){
-//         console.log(personAge[i].name + " is a teenager")
-//     }else if (personAge[i].age <13) {
-//         console.log(personAge[i].name + " is a child")
-//     }else {
-//         console.log(personAge[i].name + ' is an adult');
-//     }
-// }
 
 
 
-//  let text = "";
-//  let i = 0;
-
-//  do {
-//     text = text + " " + i;
-//     i++;
-//  }
-//  while (i < 10);
-//  console.log(text);
 
 
-// let fruits = ["apple", "banana", "cherry", "elderberry"]
-// let text = "";
-// let i = 0;
-
-// while (fruits[i]) {
-//     text = text + " " + fruits[i];
-//     i++;
-// }
-
-// console.log(text);
-    
-// let fruits = ["apple", "banana", "cherry", "elderberry"]
-// let text = "";
-// let i = 0;
-// for (;fruits[i];) {
-//     text = text + " " + fruits[i];
-//     i++;
-// }
-
-// console.log(text);
