8e9b6277c1020ce4f54cd783a3e2fdb2b92b19a1
CB <carlo_bregy@outlook.com>
2025-07-04 14:49:50 +0200

    Deleted Comments

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,17 @@ var pokemonList = [{ name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] }
 for (let i = 0; i < pokemonList.length; i++){
     document.write(pokemonList[i].name + " " + (pokemonList[i].height));
     if (pokemonList[i].height >2 ) {
-        document.write(" - This is big");     // If the height is greater than 2, print a message
+        document.write(" - This is big");    
         }
-        document.write("<br>");               // Move to next line after each Pokémon
-    }
+        document.write("<br>");           
 
 
 
 
+let myArray = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
 
 
 
